c0d2cd59858bee620df1e35921a85d5359e30500
Nitin bhatia <nitin.bhatia@soprasteria.com>
2020-04-02 10:36:13 +0530

    Master Branch Checkin

---
+++ WebApplicationGit/WebApplicationGit/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplicationGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
